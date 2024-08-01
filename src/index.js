09ccf2eca184f00eaf1f09f4d474a0e5a5a00f24
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-08-01 10:43:09 +0530

    mongodb setup done

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({path:'./env'})
+
+connectDB()
+
+
+
+
+
+
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants"
+
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+//         app.on("error",(error)=>{
+//             console.log("Error: ",error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`server listening on ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.log("Error: ",error)
+//         throw error
+//     }
+// })()
