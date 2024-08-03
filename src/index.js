1a3d5979534d40d2f3fdaa48f870cce618288313
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-08-03 23:43:43 +0530

    apiError file set

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import express from "express"
 
 dotenv.config({path:'./env'})
 
+const port = process.env.PORT || 3000
+const app=express()
+
 connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is running on ${port}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection failed",error)
+})
 
 
 
