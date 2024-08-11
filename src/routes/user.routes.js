330acce064844187907f17d55bd8dae57f3fdefa
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-08-11 23:54:16 +0530

    regester api all bug solved

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -12,7 +12,7 @@ router.route("/register").post(
     },
     {
       name: "coverImage",
-      maxCount: 1, 
+      maxCount: 1,
     }
   ]),
   registerUser
